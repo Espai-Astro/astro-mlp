fbad49d3339d2b9b841371678c85539b67e7e76c
llopFilms <llopFilms@gmail.com>
2023-10-24 20:30:59 +0200

    Updates

--- pages/biografia.astro.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('../chunks/pages/biografia_ecb33c88.mjs');
-
-export { page };
