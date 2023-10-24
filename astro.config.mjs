3cc4aa9ecd54c618ce07b0637f05aca66e09377e
llopFilms <llopFilms@gmail.com>
2023-10-24 17:57:04 +0200

    Actualització

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://espai-astro.github.io',
   base: '/astro-mlp',
-  output: 'server',
+  output: "server",
+  adapter: vercel()
 });
