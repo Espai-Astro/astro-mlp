30f33684cc744ab5cd816ff0e211b0aade4f5d84
llopFilms <llopFilms@gmail.com>
2023-10-24 18:15:05 +0200

    Actualització

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,4 +1,3 @@
 import { textosInici, galeria, xxss, textosBiografia } from './contingutWeb';
-import { endPoints } from '../const/endPoints';
 
-export { textosInici, galeria, endPoints, xxss, textosBiografia };
+export { textosInici, galeria, xxss, textosBiografia };
