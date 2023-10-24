dcb6c53632c299cb76e468a59261f111c1e423f5
llopFilms <llopFilms@gmail.com>
2023-10-24 16:14:00 +0200

    Actualització

---
+++ src/assets/index.ts
@@ -0,0 +1,58 @@
+import art1 from '../assets/img/art/art1.jpg';
+import art2 from '../assets/img/art/art2.jpg';
+import art3 from '../assets/img/art/art3.jpg';
+import art4 from '../assets/img/art/art4.jpg';
+import art5 from '../assets/img/art/art5.jpg';
+import art6 from '../assets/img/art/art6.jpg';
+import art7 from '../assets/img/art/art7.jpg';
+import art8 from '../assets/img/art/art8.jpeg';
+
+import bio1 from '../assets/img/biografia/bio1.jpg';
+import bio2 from '../assets/img/biografia/bio2.jpg';
+import bio3 from '../assets/img/biografia/bio3.jpg';
+import bio4 from '../assets/img/biografia/bio4.jpg';
+import bio5 from '../assets/img/biografia/bio5.jpg';
+import bio6 from '../assets/img/biografia/bio6.jpg';
+import bio7 from '../assets/img/biografia/bio7.jpg';
+import bio8 from '../assets/img/biografia/bio8.jpg';
+import bio9 from '../assets/img/biografia/bio9.jpg';
+import bio10 from '../assets/img/biografia/bio10.jpg';
+import bio11 from '../assets/img/biografia/bio11.jpg';
+import bio12 from '../assets/img/biografia/bio12.jpg';
+
+import avatar from '../assets/img/avatar.jpeg';
+import retrat from '../assets/img/main-portrait.jpg';
+import x from '../assets/img/twitter.png';
+import youtube from '../assets/img/youtube.png';
+
+import trass from '../assets/svg/paint.svg';
+import gargot from '../assets/svg/scribble.svg';
+
+export {
+  art1,
+  art2,
+  art3,
+  art4,
+  art5,
+  art6,
+  art7,
+  art8,
+  bio1,
+  bio2,
+  bio3,
+  bio4,
+  bio5,
+  bio6,
+  bio7,
+  bio8,
+  bio9,
+  bio10,
+  bio11,
+  bio12,
+  avatar,
+  retrat,
+  x,
+  youtube,
+  trass,
+  gargot,
+};
